32f325f17045fb9c11b50beee2ee9603a00ed3fc
QiaoRong <cn_lqr@outlook.com>
2026-01-23 00:56:36 +0800

    update

--- cpp/dynamicProgramming/hj21查找两个字符串a,b中的最长公共子串.cpp
+++ cpp/dynamicProgramming/hj21查找两个字符串a,b中的最长公共子串.cpp
@@ -1,32 +1,29 @@
 #include <bits/stdc++.h> //solo
-#include <unordered_map>
-#include <vector>
 using namespace std;
 
 void fun(string& a, string& b){
-    int na = a.size(); 
-    int nb = b.size();
-    string ls, mr;
+    int aLen = a.size(); 
+    int bLen = b.size();
+    string sortStr, longStr;
     int more, less;
-    if(na < nb){ //比较长度，记录大小
-        less = na;
-        more = nb;
-        ls = a;
-        mr = b;
-    } else {
-        less = nb;
-        more = na;
-        ls = b;
-        mr = a;
-    }
-    vector<vector<int>> dp(less+1, vector<int>(more+1, 0));
-    int maxLen = 0;
-    map<int, int> posLen;
     
-    // 先遍历短字符串
+    //①找出短字符串
+    if(aLen < bLen){ 
+        less = aLen; more = bLen;
+        sortStr = a; longStr = b;
+    } else {
+        less = bLen; more = aLen;
+        sortStr = b; longStr = a;
+    }
+
+    //②dp数组构建，生成有效数据
+    vector<vector<int>> dp(less+1, vector<int>(more+1, 0));
+    map<int, int> posLen; //记录子串 起始位置 和 切割长度
+    int maxLen = 0;
+    
     for(int i = 0; i < less; ++i){
         for(int j = 0; j < more; ++j){
-            if(ls[i] == mr[j]){
+            if(sortStr[i] == longStr[j]){
                 dp[i+1][j+1] = dp[i][j]+1;
                 if(maxLen <= dp[i+1][j+1]){
                     maxLen = dp[i+1][j+1];
@@ -39,6 +36,8 @@ void fun(string& a, string& b){
             }
         }
     }
+
+    //③查找最终结果
     int start = less-1;
     for(auto e:posLen){
         if(e.second == maxLen){
@@ -47,7 +46,7 @@ void fun(string& a, string& b){
             }
         }
     }
-    cout << ls.substr(start, maxLen);
+    cout << sortStr.substr(start, maxLen);
 }
 
 int main() {
